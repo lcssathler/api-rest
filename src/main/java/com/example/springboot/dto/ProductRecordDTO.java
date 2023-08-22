fa9a07bfa88ca39964b891735d0c4687951d0ca9
Lucas Sathler <lcssathler@gmail.com>
2023-08-21 21:58:43 -0300

    Installing dependencies and creating POST method

---
+++ src/main/java/com/example/springboot/dto/ProductRecordDTO.java
@@ -0,0 +1,10 @@
+package com.example.springboot.dto;
+
+import com.example.springboot.model.ProductModel;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+import java.math.BigDecimal;
+
+public record ProductRecordDTO(@NotBlank String name, @NotNull BigDecimal value) {
+}
---
+++ src/main/java/com/example/springboot/model/ProductModel.java
@@ -0,0 +1,41 @@
+package com.example.springboot.model;
+
+import jakarta.persistence.*;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+
+@Entity @Table(name = "TB_PRODUCTS")
+public class ProductModel {
+    private static final long SERIAL_VERSION_UID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID idProduct;
+    private String name;
+    private BigDecimal value;
+
+    public UUID getIdProduct() {
+        return idProduct;
+    }
+
+    public void setIdProduct(UUID idProduct) {
+        this.idProduct = idProduct;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public BigDecimal getValue() {
+        return value;
+    }
+
+    public void setValue(BigDecimal value) {
+        this.value = value;
+    }
+}
